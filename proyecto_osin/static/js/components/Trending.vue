ba31e5149fe573fb03497165d60a223b34be3787
omar\pc <omarx372@hotmail.com>
2018-09-04 15:18:29 -0500

    ht2

--- proyecto_osin/static/js/components/Trending.vue
+++ proyecto_osin/static/js/components/Trending.vue
@@ -4,77 +4,77 @@
           <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
          <div class="form-group">
               <label for="persona">ALGERIA</label>
-              <select class="form-control input-sm">
+              <select class="form-control input-sm" v-model="combo_algeria" @change="gettrending()">
                  <option value="">Seleccione</option>
                  <option v-for="sel in options_algeria" :value="sel.id">{{sel.nombre}}</option>
                </select>         
          </div>
          <div class="form-group">
               <label for="persona">ARGENTINA</label>
-              <select class="form-control input-sm">
+              <select class="form-control input-sm" v-model="combo_argentina">
                  <option value="">Seleccione</option>
                  <option v-for="sel in options_argentina" :value="sel.id">{{sel.nombre}}</option>
                </select>         
          </div>
           <div class="form-group">
               <label for="persona">AUSTRALIA</label>
-              <select class="form-control input-sm">
+              <select class="form-control input-sm" v-model="combo_australia">
                  <option value="">Seleccione</option>
                  <option v-for="sel in options_australia" :value="sel.id">{{sel.nombre}}</option>
                </select>         
          </div>
          <div class="form-group">
               <label for="persona">AUSTRIA</label>
-              <select class="form-control input-sm">
+              <select class="form-control input-sm" v-model="combo_austria"> 
                  <option value="">Seleccione</option>
                  <option v-for="sel in options_austria" :value="sel.id">{{sel.nombre}}</option>
                </select>         
          </div>
           <div class="form-group">
               <label for="persona">BRAZIL</label>
-              <select class="form-control input-sm">
+              <select class="form-control input-sm" v-model="combo_brazil">
                  <option value="">Seleccione</option>
                  <option v-for="sel in options_brasil" :value="sel.id">{{sel.nombre}}</option>
                </select>         
          </div>
           <div class="form-group">
               <label for="persona">CHILE</label>
-              <select class="form-control input-sm">
+              <select class="form-control input-sm" v-model="combo_chile">
                  <option value="">Seleccione</option>
                  <option v-for="sel in options_chile" :value="sel.id">{{sel.nombre}}</option>
                </select>         
          </div>
            <div class="form-group">
               <label for="persona">COLOMBIA</label>
-              <select class="form-control input-sm">
+              <select class="form-control input-sm" v-model="combo_colombia">
                  <option value="">Seleccione</option>
                  <option v-for="sel in options_colombia" :value="sel.id">{{sel.nombre}}</option>
                </select>         
          </div>
           <div class="form-group">
               <label for="persona">ECUADOR</label>
-              <select class="form-control input-sm">
+              <select class="form-control input-sm" v-model="combo_ecuador">
                  <option value="">Seleccione</option>
                  <option v-for="sel in options_ecuador" :value="sel.id">{{sel.nombre}}</option>
                </select>         
          </div>
           <div class="form-group">
               <label for="persona">MEXICO</label>
-              <select class="form-control input-sm">
+              <select class="form-control input-sm" v-model="combo_mexico">
                  <option value="">Seleccione</option>
                  <option v-for="sel in options_mexico" :value="sel.id">{{sel.nombre}}</option>
                </select>         
          </div>
            <div class="form-group">
               <label for="persona">PERÚ</label>
-              <select class="form-control input-sm">
+              <select class="form-control input-sm" v-model="combo_peru">
                  <option value="">Seleccione</option>
                  <option v-for="sel in options_peru" :value="sel.id">{{sel.nombre}}</option>
                </select>         
          </div>
            <div class="form-group">
               <label for="persona">ESPAÑA</label>
-              <select class="form-control input-sm">
+              <select class="form-control input-sm" v-model="combo_spain"> 
                  <option value="">Seleccione</option>
                  <option v-for="sel in options_spain" :value="sel.id">{{sel.nombre}}</option>
                </select>         
@@ -83,30 +83,69 @@
          
           </div>
           <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
-                  
+             <div class="form-group" v-show="loading">
+              <i class="fa fa-spinner fa-spin" style="font-size:48px"></i> Cargando....
+            </div> 
+                  <div class="panel-body div1 table-responsive">
+            <table class="table table-bordered table-hover table-fixed table-striped" style="background:white;">
+                 <thead>
+                 <tr>
+                     <th>#</th>
+                     <th>Trending Topic Global</th>
+                 </tr>
+                 </thead>
+                  <tbody>
+                     <template v-if="lista!=''">
+                  <tr v-for="(list,index) in lista">
+                      <td>{{index+1}}</td>
+                      <td>{{list}}</td>
+                  </tr>
+                       </template>
+                       <template v-else>
+                      	<tr>
+        	          <td colspan="2" align="center">No hay resultados disponibles</td>	
+        	          </tr>
+                     </template> 
+                  </tbody>         
+            </table>
+                  </div> 
           </div>
      </div>
 </div>
 </template>
 <script>
+
  export default{
      created(){
-       
+       this.gettrending();
      },
      data(){
          return{
-          'options_algeria':[
+            lista:[], 
+            loading: false,
+            combo_algeria:'',
+            combo_argentina:'',
+            combo_australia:'',
+            combo_austria:'',
+            combo_brazil:'',
+            combo_chile:'',
+            combo_colombia:'',
+            combo_ecuador:'',
+            combo_mexico:'',
+            combo_peru:'',
+            combo_spain:'', 
+            options_algeria:[
  		     { id: 'algeria', nombre: 'ALGERIA'},
  		     { id: 'algeria/algiers', nombre: 'ALGIERS'}
             ],
-           'options_argentina':[
+           options_argentina:[
              { id: 'argentina', nombre: 'ARGENTINA'},
              { id: 'argentina/buenos-aires', nombre: 'BUENOS AIRES'},
              { id: 'argentina/cordoba', nombre: 'CÓRDOBA'},
              { id: 'argentina/mendoza', nombre: 'MENDOZA'},
              { id: 'argentina/rosario', nombre: 'ROSARIO'}
            ],
-           'options_australia':[
+           options_australia:[
              { id: 'australia', nombre: 'AUSTRALIA'},
              { id: 'australia/adelaide', nombre: 'ADELAIDE'},
              { id: 'australia/brisbane', nombre: 'BRISBANE'},
@@ -116,11 +155,11 @@
              { id: 'australia/perth', nombre: 'PERTH'},
              { id: 'australia/sydney', nombre: 'SYDNEY'}
            ],
-           'options_austria':[
+            options_austria:[
              { id: 'austria', nombre: 'AUSTRIA'},
              { id: 'austria/vienna', nombre: 'VIENNA'}, 
            ],
-           'options_brasil':[
+           options_brasil:[
              { id: 'brazil', nombre: 'BRAZIL'},
              { id: 'brazil/belem', nombre: 'BELÉM'}, 
              { id: 'brazil/belo-horizonte', nombre: 'BELO HORIZONTE'},
@@ -134,25 +173,25 @@
              { id: 'brazil/salvador', nombre: 'SALVADOR'},
              { id: 'brazil/sao-paulo', nombre: 'SAO PAULO'}
            ],
-           'options_chile':[
+           options_chile:[
              { id: 'chile', nombre: 'CHILE'},
              { id: 'chile/concepcion', nombre: 'CONCEPCIÓN'}, 
              { id: 'chile/santiago', nombre: 'SANTIAGO'},
              { id: 'chile/valparaiso', nombre: 'VALPARAISO'}
            ],
-           'options_colombia':[
+           options_colombia:[
              { id: 'colombia', nombre: 'COLOMBIA'},
              { id: 'colombia/barranquilla', nombre: 'BARRANQUILLA'}, 
              { id: 'colombia/bogota', nombre: 'BOGOTÁ'},
              { id: 'colombia/cali', nombre: 'CALI'},
              { id: 'colombia/medellin', nombre: 'MEDELLÍN'}  
            ],
-           'options_ecuador':[
+           options_ecuador:[
              { id: 'ecuador', nombre: 'ECUADOR'},
              { id: 'ecuador/guayaquil', nombre: 'GUAYAQUIL'}, 
              { id: 'ecuador/quito', nombre: 'QUITO'}
            ],
-           'options_mexico':[
+           options_mexico:[
              { id: 'mexico/', nombre: 'MEXICO'},
              { id: 'mexico/acapulco', nombre: 'ACAPULCO'}, 
              { id: 'mexico/aguascalientes', nombre: 'AGUASCALIENTES'},
@@ -178,11 +217,11 @@
              { id: 'mexico/toluca', nombre: 'TOLUCA'}, 
              { id: 'mexico/zapopan', nombre: 'ZAPOPAN'}
            ],
-           'options_peru':[
+           options_peru:[
              { id: 'peru', nombre: 'PERÚ'},
              { id: 'peru/lima', nombre: 'LIMA'}
            ],
-           'options_spain':[
+           options_spain:[
              { id: 'spain', nombre: 'ESPAÑA'},
              { id: 'spain/barcelona', nombre: 'BARCELONA'},
              { id: 'spain/madrid', nombre: 'MADRID'},
@@ -193,11 +232,30 @@
          }
      },
      methods:{
-     
-     }
-     
+             gettrending(){
+              let buscador=this.combo_algeria;
+              this.loading = true;
+                     axios.get('http://127.0.0.1:8000/search/gettrending',{ 
+                            params: { 
+                                     buscador: buscador.trim()
+                                    }
+                        })
+                        .then(response=>{ 
+                            this.lista=response.data.info_trending;
+                            this.loading = false;       
+                        })
+                        .catch(error=>{
+                          console.log(error);
+                        })       
+           }
+     }   
  }
 </script>
 <style>
-
+.panel-body.div1.table-responsive{
+     border: 1px solid #2a3f54;
+    -webkit-box-shadow: 10px 10px 5px 0px rgba(0,0,0,0.75);
+    -moz-box-shadow: 10px 10px 5px 0px rgba(0,0,0,0.75);
+     box-shadow: 10px 10px 5px 0px rgba(0,0,0,0.75);
+} 
 </style>
